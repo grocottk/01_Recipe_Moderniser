aa633175f55f8ef2e6ac439067fbe4ab35097365
grocottk <51727305+grocottk@users.noreply.github.com>
2020-02-14 14:30:13 +1300

    Recipe Name Component with Blank Checcker
    Blank Checker has been added, however, number checker has not.

---
+++ 01_recipe_name.py
@@ -0,0 +1,25 @@
+
+# Recipe Moderniser Component 1
+# Asks user for the name of their recipe, and checks that it is not blank and does not have numbers.
+
+# Blank Checking Function:
+
+def not_blank(question):
+
+    valid = False
+    while not valid
+        response = input(question)
+
+        if response == "":
+            continue
+        else:
+            return response
+
+        return response
+
+# Main Routine:
+
+recipe_name = not_blank("What is the name of your recipe? ")
+
+print("You are making {}".format(recipe_name))
+
