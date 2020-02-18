d9b99aa47f1e487153ce749ec4019d69db1e9574
grocottk <51727305+grocottk@users.noreply.github.com>
2020-02-19 10:27:57 +1300

    Final version of Recipe Name Component (Component 1)
    The final, tested version of the first component in the Recipe Moderniser Program

--- 01_recipe_name.py
+++ 01_recipe_name.py
@@ -4,41 +4,42 @@
 
 # Blank Checking Function:
 
+
 def not_blank(question):
 
     error = "Sorry, but you must enter a string as your recipe name. You cannot have numbers in your recipe name."
 
-    has_errors = ""
-
     valid = False
     while not valid:
         response = input(question)
+        has_errors = ""
 
         # Look at each character in string, if any characters are numbers, give an error
 
         for letter in response:
             if __name__ == '__main__':
-                if letter.isdigit() == True:
+                if letter.isdigit():
                     has_errors = "yes"
                     break
 
         if response == "":
             print()
+            print("Sorry, but you must enter something as your recipe name. You cannot leave this blank.")
+            print()
             continue
 
         elif has_errors != "":
+            print()
             print(error)
+            print()
             continue
 
         else:
             return response
 
-        return response
-
-
-
 # Main Routine:
 
 recipe_name = not_blank("What is the name of your recipe? ")
 
+print()
 print("You are making {}".format(recipe_name))
