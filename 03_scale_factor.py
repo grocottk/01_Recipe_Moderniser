919aa00785eacbd968c91cae54b1e127450ab227
grocottk <51727305+grocottk@users.noreply.github.com>
2020-02-21 14:50:15 +1300

    Implementing Number Checking Function
    I have now (mostly) implemented the number checking function into Component 3

--- 03_scale_factor.py
+++ 03_scale_factor.py
@@ -4,12 +4,25 @@
 
 # Functions:
 
+# Number Checking Function
+
+def number_checker(question):
+
+    error = "You cannot enter a number that is zero or less. Please try again"
+
+    valid = False
+    while not valid:
+        try:
+            response = float(input(question))
+
+        except ValueError:
+            print(error)
 
 # Main Routine:
 
-current_size = float(input("How many servings does the recipe currently make? "))
+current_size = float(number_checker("How many servings does the recipe currently make? "))
 print()
-desired_size = float(input("How many servings would you like to make? "))
+desired_size = float(number_checker("How many servings would you like to make? "))
 print()
 
 scale_factor = desired_size / current_size
