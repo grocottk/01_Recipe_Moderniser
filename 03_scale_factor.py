35d6e730770bafee2fc617c6ab344ed978943933
grocottk <51727305+grocottk@users.noreply.github.com>
2020-02-26 10:41:09 +1300

    Final Scale Factor Version
    This is the final version of the scale factor component (Component 3), which has now been screenshotted.

--- 03_scale_factor.py
+++ 03_scale_factor.py
@@ -7,6 +7,7 @@
 
 # Number Checking Function
 
+
 def number_checker(question):
 
     error = "Please enter a number that is more than zero."
@@ -29,29 +30,32 @@ def number_checker(question):
 
 # Main Routine:
 
-ideal_scale_factor = "no"
-while ideal_scale_factor == "no":
 
-    current_size = number_checker("How many servings does the recipe currently make? ")
-    print()
+current_size = number_checker("How many servings does the recipe currently make? ")
+print()
+
+enter_scale_factor = "yes"
+while enter_scale_factor == "yes":
+
     desired_size = number_checker("How many servings would you like to make? ")
     print()
 
     scale_factor = desired_size / current_size
 
     if scale_factor < 0.25:
-        ideal_scale_factor = input("Warning: This scale factor is very small, "
+        enter_scale_factor = input("Warning: This scale factor is very small, "
                                    "which might make it hard to measure accurately. \n"
                                    "You might want to make the original recipe and keep leftovers. \n"
-                                   "Do you want to continue with this scale factor? Type no to change your "
-                                   "desired serving size. ")
+                                   "Do you want to fix this and make more servings? ").lower()
 
     elif scale_factor > 4:
-        ideal_scale_factor = input("Warning: This scale factor is very large, "
+        enter_scale_factor = input("Warning: This scale factor is very large, "
                                    "which might not scale accurately to the average kitchen. \n"
                                    "You might want to make the original recipe in multiple batches. \n"
-                                   "Do you want to continue with this scale factor? Type no to change your "
-                                   "desired serving size. ")
+                                   "Do you want to fix this and make less servings? ").lower()
+
+    else:
+        enter_scale_factor = "no"
 
 print()
 print("The scale factor of this recipe is {}".format(scale_factor))
