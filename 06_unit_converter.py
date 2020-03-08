7d81b71f61a651d24313a0a5f349389be635bcc7
grocottk <51727305+grocottk@users.noreply.github.com>
2020-03-08 21:30:50 +1300

    Component 6 with Error
    Beginning the second part of Component 6, I have encountered an error that is preventing me from running the program, and beginning Component 7.

--- 06_unit_converter.py
+++ 06_unit_converter.py
@@ -4,7 +4,80 @@
 
 # To Do List on ePUB
 
+# Functions:
+
+
+def unit_checker():
+
+    unit_to_check = input("What is your unit? ")
+
+    # Abbreviation Lists for various measurements
+
+    teaspoon = ["tsp", "teaspoon", "t"]
+    tablespoon = ["tbs", "tablespoon", "T", "tbsp"]
+    ounce = ["oz", "fluid ounce", "fl oz"]
+    cup = ["c", "cups"]
+    pint = ["p", "pt", "fl pt"]
+    quart = ["q", "qt", "fl qt"]
+    milliliter = ["millilitre", "ml", "cc", "mL"]
+    liter = ["l", "litre", "L"]
+    deciliter = ["dl", "decilitre", "dL"]
+    pound = ["lb", "lbs", "#"]
+    stick = ["knob"]
+
+    if unit_to_check == "":
+
+        print("You choose {}".format(unit_to_check))
+        return unit_to_check
+
+    elif unit_to_check == "T" or unit_to_check.lower() in tablespoon:
+
+        return "tbs"
+
+    elif unit_to_check.lower() in teaspoon:
+
+        return "tsp"
+
+    elif unit_to_check.lower() in ounce:
+
+        return "ounce"
+
+    elif unit_to_check.lower() in cup:
+
+        return "cup"
+
+    elif unit_to_check.lower() in pint:
+
+        return "pint"
+
+    elif unit_to_check.lower() in quart:
+
+        return "quart"
+
+    elif unit_to_check.lower() in milliliter:
+
+        return "milliliter"
+
+    elif unit_to_check.lower() in liter:
+
+        return "liter"
+
+    elif unit_to_check.lower() in deciliter:
+
+        return "deciliter"
+
+    elif unit_to_check.lower() in pound:
+
+        return "pound"
+
+    elif unit_to_check.lower() in stick:
+
+        return "stick"
+
+# Main Routine
+
 # Building list of milliliter unit conversions
+
 
 unit_dictionary = {
     "tsp": 5,
@@ -15,6 +88,9 @@ unit_dictionary = {
     "quart": 946,
     "pound": 454,
     "stick": 113,
+    "liter": 1000,
+    "milliliter": 1,
+    "deciliter": 100,
 }
 
 # Loop Begins
@@ -27,7 +103,9 @@ while keep_going == "":
     amount = eval(input("How much? "))
     amount = float(amount)
 
-    unit = input("Unit? ")
+    # Gets unit an changes it to match the dictionary
+
+    unit = unit_checker()
 
     # Checks if the unit is in the unit dictionary
 
