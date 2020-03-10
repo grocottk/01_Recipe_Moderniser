a766988de31bac7c48299b04a8e304d84f6a09a8
grocottk <51727305+grocottk@users.noreply.github.com>
2020-03-10 19:56:02 +1300

    Component 7 Video 2 Completion
    This is the build of the code after completing the second video in the Component 7 (Gram Converter) series. The code is currently unavailable to have decomposition screenshots taken of it, however screenshots will be added when available.

--- 07_gram_converter.py
+++ 07_gram_converter.py
@@ -25,9 +25,42 @@ food_dictionary = {}
 
 for row in csv_groceries:
 
-
     food_dictionary[row[0]] = row[1]
 
-# Prints the dictionary as a whole
+# Prints the dictionary as a whole (Useful for testing, but ultimately removed)
 
-print(food_dictionary)
+# print(food_dictionary)
+
+# Main Routine
+
+# Loop Begins
+
+keep_going = ""
+while keep_going == "":
+
+    # Evaluates how much/unit of ingredient (allows for equations to be done in converter,
+    # however does not allow fractions)
+
+    amount = eval(input("How much? "))
+    amount = float(amount)
+
+    # Gets an ingredient and changes it to be lowercase to match the dictionary
+
+    ingredient = input("Please enter your ingredient: ").lower()
+
+    # Checks if the unit is in the food dictionary
+
+    if ingredient in food_dictionary:
+
+        multiply_by = food_dictionary.get(ingredient)
+        amount = amount * float(multiply_by) / 250
+
+        # Prints the amount in grams if the ingredient is in the dictionary
+
+        print("Amount in grams: {}".format(amount))
+
+    # If the unit is not in the unit dictionary, the unit is unchanged
+
+    else:
+
+        print("{} is unchanged".format(amount))
