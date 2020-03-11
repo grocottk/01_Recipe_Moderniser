4aab3c0184335f5159730b9be45b72afe9631fc4
grocottk <51727305+grocottk@users.noreply.github.com>
2020-03-11 21:09:16 +1300

    Input Splitter as of First Video
    This is the build of Component 9 (Input Splitter) as of the end of the first video in relation to the subject. The code will be built on alongside the next two videos.

--- 09_ingredient_splitter.py
+++ 09_ingredient_splitter.py
@@ -8,3 +8,15 @@ import re
 # (Change below into input statement at a later date)
 
 recipe_line = "1 1/2 ml flour"
+
+mixed_regex = "\d{1,3}\s\d{1,3}\/\d{1,3}"
+
+# If the regular expression is functional, then print "Functional", if not, print "Not Functional"
+
+if re.match(mixed_regex, recipe_line):
+
+    print("Functional")
+
+else:
+
+    print("Not Functional")
