88ca8f07e133748f6e6774ed713929d4c479f2c9
grocottk <51727305+grocottk@users.noreply.github.com>
2020-03-12 09:38:43 +1300

    Component 9 at end of Video 2
    This is the build of the code by the end of Video 2 of 3. Decomposition screenshots have been taken of this build, and work will now begin on Video 3

--- 09_ingredient_splitter.py
+++ 09_ingredient_splitter.py
@@ -17,6 +17,64 @@ if re.match(mixed_regex, recipe_line):
 
     print("Functional")
 
-else:
+    # Get mixed number by matching the regular expression
 
-    print("Not Functional")
+    pre_mixed_number = re.match(mixed_regex, recipe_line)
+
+    # Using the group method to isolare the number
+
+    mixed_number = pre_mixed_number.group()
+
+    # Replace the space with a 'plus' sign:
+
+    amount = mixed_number.replace(" ", "+")
+
+    # Change the above string into a decimal:
+
+    amount = eval(amount)
+
+    print(amount)
+
+    # Get Unit and ingredient:
+
+    compile_regex = re.compile(mixed_regex)
+
+    # Prints the compiled Regular Expression
+
+    print(compile_regex)
+
+    # Inserts data gained so far into a list
+
+    unit_ingredient = re.split(compile_regex, recipe_line)
+
+    # Removes the extra whitespace (spaces) from a unit
+
+    unit_ingredient = (unit_ingredient[1]).strip()
+
+    # Prints the list
+
+    print(unit_ingredient)
+
+# Splits the text at the first space
+
+get_unit = unit_ingredient.split(" ", 1)
+
+# Prints the list, with no whitespace
+
+print(get_unit)
+
+# Sets the Unit to the first item in the list
+
+unit = get_unit[0]
+
+# Sets the Ingredient to the second item in the list
+
+ingredient = get_unit[1]
+
+# Prints the Unit
+
+print(unit)
+
+# Prints the Ingredient
+
+print(ingredient)
