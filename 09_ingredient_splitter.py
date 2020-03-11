3c453e83ee424a61b84b33503ab0738f53a34bde
grocottk <51727305+grocottk@users.noreply.github.com>
2020-03-11 20:34:35 +1300

    Renaming Assembled Outcome and Creating Component 9
    The Assembled Outcome has been renamed to begin with a 10 now, while the number 9 is now representing Component 9 (Input Splitter). Component 9 has recently been started, while no changes haven been made to the Assembled Outcome (aside from the renaming)

---
+++ 09_ingredient_splitter.py
@@ -0,0 +1,10 @@
+
+# Recipe Moderniser Component 9
+# Improves the experience of inserting information on the recipe into the program.
+
+import re
+
+# Ingredient has a mixed fraction followed by a unit and ingredient
+# (Change below into input statement at a later date)
+
+recipe_line = "1 1/2 ml flour"
